158c4b34ba5777cf3120befa3133ddbea7a187a4
SergioSusanu <Sergio.susanu.developer@gmail.com>
2023-10-19 18:10:55 +0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import InitialDataProvider from './data/InitialDataProvider';
+import CssBaseline from "@mui/material/CssBaseline";
+
 
 function App() {
   return (
     <InitialDataProvider>
+      <CssBaseline enableColorScheme />
       <div className="homepage">
         <Header />
         <Main />
